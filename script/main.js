d51eb129aeb63e9efd57b2bb181d5de9d06ebecd
Alena Timashova <alyona8808@mail.ru>
2024-02-27 23:39:18 +0300

    second hw - blog page added

--- script/main.js
+++ script/main.js
@@ -8,6 +8,7 @@ const myApp = {
           date: "26 December,2022",
           die: "Kitchan Design",
           img: "src/img/article_img1.png",
+          top: true,
         },
         {
           id: 2,
@@ -15,6 +16,7 @@ const myApp = {
           date: "22 December,2022",
           die: "Living Design",
           img: "src/img/article_img2.png",
+          top: true,
         },
         {
           id: 3,
@@ -22,9 +24,39 @@ const myApp = {
           date: "25 December,2022",
           die: "Interior Design",
           img: "src/img/article_img3.png",
+          top: true,
+        },
+        {
+          id: 4,
+          title: "Let’s Get Solution For Building Construction Work",
+          date: "26 December,2022",
+          die: "Kitchan Design",
+          img: "src/img/article_img4.png",
+          top: false,
+        },
+        {
+          id: 5,
+          title: "Low Cost Latest Invented Interior Designing \n Ideas.",
+          date: "22 December,2022",
+          die: "Living Design",
+          img: "src/img/article_img5.png",
+          top: false,
+        },
+        {
+          id: 6,
+          title: "Best For Any Office & Business Interior Solution",
+          date: "25 December,2022",
+          die: "Interior Design",
+          img: "src/img/article_img6.png",
+          top: false,
         },
       ],
     };
+  },
+  computed: {
+    topArticles: function () {
+      return this.articles.filter((article) => article.top);
+    },
   },
 };
 
